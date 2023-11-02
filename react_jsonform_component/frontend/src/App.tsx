d311eeed8833594c7935560bea7b148e0a8bc7da
Hubert Bereś <hubert@springbok.ai>
2023-11-02 23:49:14 +0000

    Render a basicmost jsonform

--- react_jsonform_component/frontend/src/App.tsx
+++ react_jsonform_component/frontend/src/App.tsx
@@ -1,32 +1,25 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode } from "react";
 import {
-  Streamlit,
   StreamlitComponentBase,
   withStreamlitConnection,
-} from "streamlit-component-lib"
+} from "streamlit-component-lib";
 
-interface State {
-  numClicks: number
-  isFocused: boolean
-}
+import Form from "@rjsf/core";
+import { RJSFSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
-class JsonformComponent extends StreamlitComponentBase<State> {
-    public state = { numClicks: 0, isFocused: false }
-    public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
-    const name = this.props.args["name"]
-    // useEffect(() => Streamlit.setFrameHeight())
+const schema: RJSFSchema = {
+  title: "Test form",
+  type: "string",
+};
 
-    // Streamlit sends us a theme object via props that we can use to ensure
-    // that our component has visuals that match the active theme in a
-    // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
+class JsonformComponent extends StreamlitComponentBase {
+  public state = { numClicks: 0, isFocused: false };
+  public render = (): ReactNode => {
+    const { theme } = this.props;
+    // const { name } = this.props.args;
+
+    const style: React.CSSProperties = {};
 
     // Maintain compatibility with older versions of Streamlit that don't send
     // a theme object.
@@ -35,55 +28,13 @@ class JsonformComponent extends StreamlitComponentBase<State> {
       // theme style is defined in CSS vars.
       const borderStyling = `1px solid ${
         this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
+      }`;
+      style.border = borderStyling;
+      style.outline = borderStyling;
     }
 
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
-    // variable, and send its new value back to Streamlit, where it'll
-    // be available to the Python program.
-    return (
-      <span>
-        Hello, {name}! &nbsp;
-        <button
-          style={style}
-          onClick={this.onClicked}
-          disabled={this.props.disabled}
-          onFocus={this._onFocus}
-          onBlur={this._onBlur}
-        >
-          Click Me!
-        </button>
-      </span>
-    )
-  }
-
-  /** Click handler for our "Click Me!" button. */
-  private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-    this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
-      () => Streamlit.setComponentValue(this.state.numClicks)
-    )
-  }
-
-  /** Focus handler for our "Click Me!" button. */
-  private _onFocus = (): void => {
-    this.setState({ isFocused: true })
-  }
-
-  /** Blur handler for our "Click Me!" button. */
-  private _onBlur = (): void => {
-    this.setState({ isFocused: false })
-  }
+    return <Form schema={schema} validator={validator} />;
+  };
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
-export default withStreamlitConnection(JsonformComponent)
+export default withStreamlitConnection(JsonformComponent);
