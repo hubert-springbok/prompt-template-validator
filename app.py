20d3794670364e7196484c0e0df4b6d073770f22
Hubert Bereś <hubert@springbok.ai>
2023-11-02 18:09:48 +0000

    File pickers

---
+++ app.py
@@ -0,0 +1,38 @@
+"""
+Streamlit Cheat Sheet
+"""
+
+import streamlit as st
+
+# Initial page config
+
+st.set_page_config(
+     page_title='Streamlit cheat sheet',
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
+
+def main():
+    cs_sidebar()
+    cs_body()
+
+    return None
+
+def cs_sidebar():
+    st.header("Streamlit app to preview prompt templates")
+    
+def cs_body():
+    schema = st.file_uploader("Upload a schema file", type=["json", "yaml"], key="schema_file", accept_multiple_files=False)
+    template = st.file_uploader("Upload a template file", type=["text", "jinja2"], key="template_file", accept_multiple_files=False)
+
+    if schema and template:
+        st.write("Files uploaded")
+        st.write(schema)
+        st.write(template)
+        st.write("Now you can generate the report")
+        if st.button("Generate report"):
+            st.write("Report generated")
+            st.balloons()
+
+if __name__ == '__main__':
+    main()
